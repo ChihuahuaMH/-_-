245dd0639affe7b00b42aa21074192a043ca881f
ChihuahuaMH <hsien1126@gmail.com>
2020-05-22 11:10:22 +0800

    0522 路徑修改

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+     
+      
+        <button type="button" className="btn btn-info">Go!</button>
+      
+    </div>
+  );
+}
+
+export default App;
